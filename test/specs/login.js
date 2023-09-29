c64d8dc0d1f99cd13c5edd3870e72b081c7a0817
User <user@192.168.1.6>
2023-09-29 14:11:25 +0530

    login and pageboject login

--- test/specs/login.js
+++ test/specs/login.js
@@ -1,26 +1,31 @@
-describe("login",()=>
-{
-    it("should select language and move to login",async()=>
-    {
+const login = require('../pageobjects/page.login');
+describe("login", () => {
+    it("should select language and move to login", async () => {
         await driver.pause(3000);
-        await $('android=new UiScrollable(new UiSelector().scrollable(true)).scrollTextIntoView("English")');
-         language = await $('//*[@text="English"]');
-         await language.click();
-         let  okbutton = await $('//*[@resource-id="com.smule.singandroid:id/yesButton"]');
-         await okbutton.click();
+        await login.uiScrollableBtn;
+        language = await login.languageBtn;
+        await language.click();
+        let okbutton = await login.okBtn;
+        await okbutton.click();
     })
 
 
-    it("email ")
+    it("email should be invalid and next buttion should be disable", async () => {
+        await login.signInBtn.click();
+        await login.setValueEmailBtn.setValue('harshit25');
+        // let nextBtn=);
+        expect(await login.nextBtn).not.toBeClickable();
+    })
 
-    it("click on sign with email and get login",async()=>
-    {
-        await $('//*[@resource-id="com.smule.singandroid:id/email_button_view"]').click();
-        await $('//*[@text="Email Address"]').addValue('harshit25@tv.com');
-        await $('//*[@resource-id="com.smule.singandroid:id/btn_next"]').click();
-        await $('//*[@text="your password"]').addValue("harshit25");
-        await $('//*[@text="Next"]').click();
+    it("click on sign with email and get login", async () => {
+        // await $('//*[@resource-id="com.smule.singandroid:id/email_button_view"]').click();
+        await login.setValueEmailBtn.setValue('harshit25@tv.com');
+        // await $('//*[@resource-id="com.smule.singandroid:id/btn_next"]').click();
+        await login.nextBtn.click();
+        await login.passwdBtn.addValue("harshit25");
+        await login.nextBtn.click();
 
-    
+        await driver.pause(5000)
+
     })
 })
